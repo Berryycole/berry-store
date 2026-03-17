76c77eaaa0511990a48f028a770ecc2b7f66398b
Nicole-Fernandez05 <202411303@fit.edu.ph>
2026-03-17 20:17:14 +0800

    Fixed backend image paths and updated documentation text

--- react-ecommerce/src/pages/Home.jsx
+++ react-ecommerce/src/pages/Home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
              <div style={{ 
                height: '100%', 
                width: '100%',
-               backgroundImage: `url("https://healthyhenrycounty.org/wp-content/uploads/2020/08/healthy-lifestyle-1-1024x585.png")`, 
+               backgroundImage: `url("https://i.ibb.co/KxSJfFS0/Eat-Healthy-Live-Better.png")`, 
                backgroundSize: 'cover', 
                backgroundPosition: 'center', 
                display: 'flex', 
